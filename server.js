e02fffe8f4b586020eb2f90362a9bed4dd057604
Simaratravel <sk645669@gmail.com>
2026-02-11 09:49:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Simara Support System API is running 🚀");
 });
+const indexRoutes = require("./routes/index.js");
+const testRoutes = require("./routes/testRoutes.js");
+app.use("/", indexRoutes);
+app.use("/test", testRoutes);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
