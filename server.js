68ea1dc737473123177c3d52fe85c57cacdfc4af
Simaratravel <sk645669@gmail.com>
2026-02-11 09:31:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,5 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Simara Support System API is running 🚀");
 });
-
-const PORT = process.env.PORT || 3000;
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
